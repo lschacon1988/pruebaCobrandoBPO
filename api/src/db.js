7748d45d95663e3b5af47f8a532c9ea62fd64e5b
lschacon1988 <lschacon1988@gmail.com>
2022-10-05 01:47:23 -0500

    update .env

--- api/src/db.js
+++ api/src/db.js
@@ -1,14 +1,14 @@
 import Sequelize from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
-const { DB_USER, DB_PASSWORD, DB_HOST,DATABASE } = process.env;
+const {  PGUSER,  PGPASSWORD,  PGHOST, PGDATABASE } = process.env;
 
 export const sequelize = new Sequelize(
-DATABASE, 
-DB_USER, 
-DB_PASSWORD,
+PGDATABASE, 
+PGUSER, 
+PGPASSWORD,
  {
-  host: DB_HOST,
+  host: PGHOST,
   dialect: "postgres",
 }
 );
